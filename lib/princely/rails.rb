89768edf6460a98ae4f78e0ece67e71a1f2d4460
Oleh Khomey <varyform@gmail.com>
2018-07-27 12:20:47 +0300

    Don't use prepend

--- lib/princely/rails.rb
+++ lib/princely/rails.rb
@@ -5,11 +5,11 @@ if Mime::Type.lookup_by_extension(:pdf) != 'application/pdf'
 end
 
 if defined?(Rails)
-  if Rails::VERSION::MAJOR >= 5
-    ActionController::Base.send(:prepend, Princely::PdfHelper)
-  else
+  # if Rails::VERSION::MAJOR >= 5
+    # ActionController::Base.send(:prepend, Princely::PdfHelper)
+  # else
     ActionController::Base.send(:include, Princely::PdfHelper)
-  end
+  # end
   ActionController::Base.send(:include, Princely::AssetSupport) if
    (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR > 0) ||
    (Rails::VERSION::MAJOR >= 4)
